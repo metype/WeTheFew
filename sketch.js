0c0e66ea0691ae69b771824f3dd3a6629ad13b65
Metype <39036543+metype@users.noreply.github.com>
2018-10-18 00:45:03 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,4 +5,10 @@ createCanvas(windowWidth,windowHeight);
 function draw() {
   background(58);
   rect(0,0,width,100);
+  textSize(25);
+  text("We The few", width - 30, 60);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
